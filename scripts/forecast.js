56c6d2ee94dcbb9eea4b0bca55d791b5d949c080
Elvan Hidayat <elvanhd@gmail.com>
2023-04-09 23:19:44 +0700

    get weather api call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,3 +1,14 @@
+// get weather information
+const getWeather = async (id) => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
 // get city information
 const getCity = async (city) => {
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -10,5 +21,10 @@ const getCity = async (city) => {
 };
 
 getCity('manchester')
-  .then((data) => console.log(data))
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then((data) => {
+    console.log(data);
+  })
   .catch((err) => console.log(err));
